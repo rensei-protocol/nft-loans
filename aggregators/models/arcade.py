11c47b994909c2ec605e3978e82c44e2b5ede87c
readmost <rauan.amangeldiyev@nu.edu.kz>
2023-02-15 18:40:04 +0600

    feat: added pylinter

--- aggregators/models/arcade.py
+++ aggregators/models/arcade.py
@@ -1,29 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
 class ArcadeLoan(models.Model):
-	loan_id = models.IntegerField(primary_key=True)
-	block_time = models.DateTimeField(blank=True, null=True)
-	block_number = models.TextField(blank=True, null=True)
-	borrower = models.TextField(blank=True, null=True)
-	lender = models.TextField(blank=True, null=True)
-	borrow_amount = models.TextField(blank=True, null=True)
-	borrow_asset = models.TextField(blank=True, null=True)
-	loan_duration = models.TextField(blank=True, null=True)
-	loan_start = models.DateTimeField(blank=True, null=True)
-	loan_end = models.DateTimeField(blank=True, null=True)
-	loan_repay_amount = models.TextField(blank=True, null=True)
-	nft_asset = models.TextField(blank=True, null=True)
-	nft_token_id = models.TextField(blank=True, null=True)
-	txhash = models.TextField(blank=True, null=True)
-	state = models.TextField(blank=True, null=True)
-	num_installments = models.IntegerField(blank=True, null=True)
-	num_installments_paid = models.IntegerField(blank=True, null=True)
-	balance = models.TextField(blank=True, null=True)
-	balance_paid = models.TextField(blank=True, null=True)
-	late_fees_accrued = models.TextField(blank=True, null=True)
-	interest_rate = models.TextField(blank=True, null=True)
+    loan_id = models.IntegerField(primary_key=True)
+    block_time = models.DateTimeField(blank=True, null=True)
+    block_number = models.TextField(blank=True, null=True)
+    borrower = models.TextField(blank=True, null=True)
+    lender = models.TextField(blank=True, null=True)
+    borrow_amount = models.TextField(blank=True, null=True)
+    borrow_asset = models.TextField(blank=True, null=True)
+    loan_duration = models.TextField(blank=True, null=True)
+    loan_start = models.DateTimeField(blank=True, null=True)
+    loan_end = models.DateTimeField(blank=True, null=True)
+    loan_repay_amount = models.TextField(blank=True, null=True)
+    nft_asset = models.TextField(blank=True, null=True)
+    nft_token_id = models.TextField(blank=True, null=True)
+    txhash = models.TextField(blank=True, null=True)
+    state = models.TextField(blank=True, null=True)
+    num_installments = models.IntegerField(blank=True, null=True)
+    num_installments_paid = models.IntegerField(blank=True, null=True)
+    balance = models.TextField(blank=True, null=True)
+    balance_paid = models.TextField(blank=True, null=True)
+    late_fees_accrued = models.TextField(blank=True, null=True)
+    interest_rate = models.TextField(blank=True, null=True)
 
-	# class Meta:
-	# 	managed = False
-	# 	db_table = 'arcade_loans'
+    # class Meta:
+    # 	managed = False
+    # 	db_table = 'arcade_loans'
--- nft_loans/wsgi.py
+++ nft_loans/wsgi.py
@@ -12,8 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nft_loans.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nft_loans.settings")
 
-settings.configure(default_settings='nft_loans.settings')
+settings.configure(default_settings="nft_loans.settings")
 
 application = get_wsgi_application()
