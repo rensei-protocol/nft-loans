093d231e87a6eeae1aaac0f51d69bf22f0ef883b
readmost <rauan.amangeldiyev@nu.edu.kz>
2023-02-18 16:49:21 +0600

    feat: add gitHub actions to auto-deployment

--- aggregators/offers/x2y2/main.py
+++ aggregators/offers/x2y2/main.py
@@ -4,7 +4,7 @@ import requests
 from aggregators.models import X2Y2Offer
 
 
-class X2Y2OffchainFetcher():
+class X2Y2OffchainFetcher:
     model = X2Y2Offer
     BASE_URL = "https://loan-api.x2y2.org/v1/offer/list"
     # todo make a config file for this
@@ -16,23 +16,33 @@ class X2Y2OffchainFetcher():
         "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
         "0xED5AF388653567Af2F388E6224dC7C4b3241C544",
         "0xED5AF388653567Af2F388E6224dC7C4b3241C544",
-        "0xBd3531dA5CF5857e7CfAA92426877b022e612cf8"
+        "0xBd3531dA5CF5857e7CfAA92426877b022e612cf8",
     ]
     API_KEY = os.getenv("X2Y2_API_KEY")
 
     def get_offers(self):
-        headers = {'X-API-KEY': self.API_KEY, 'accept': 'application/json'}
+        headers = {"X-API-KEY": self.API_KEY, "accept": "application/json"}
         offers = []
         for addr in self.collections:
             more_page = True
             page = 1
             while more_page:
-                params = {'nftAddress': addr, 'tokenId': '1', 'pageSize': '20', 'page': page}
-                result = requests.get(self.BASE_URL, headers=headers, params=params)
-                data = result.json()['data']
-                more_page = data['more_page']
+                params = {
+                    "nftAddress": addr,
+                    "tokenId": "1",
+                    "pageSize": "20",
+                    "page": page,
+                }
+                result = requests.get(
+                    self.BASE_URL,
+                    headers=headers,
+                    params=params,
+                    timeout=5,
+                )
+                data = result.json()["data"]
+                more_page = data["more_page"]
                 page += 1
-                items = data['list']
+                items = data["list"]
                 for x in items:
                     x2y2_offer = X2Y2Offer(
                         offer_id=x["offerId"],
@@ -49,18 +59,14 @@ class X2Y2OffchainFetcher():
                         nonce=x["nonce"],
                         signature=x["signature"],
                         status=x["status"],
-                        create_time=x["createTime"]
+                        create_time=x["createTime"],
                     )
                     offers.append(x2y2_offer)
         return offers
 
     def process_offers(self):
         raw_offers = self.get_offers()
-        X2Y2Offer.objects.bulk_create(
-            raw_offers,
-            500,
-            True
-        )
+        X2Y2Offer.objects.bulk_create(raw_offers, 500, True)
 
     def handle(self):
         self.process_offers()
