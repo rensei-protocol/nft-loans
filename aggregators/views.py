7a266b7092b0ca3b4a3d22361f87346fd2e6ea20
0xtuba <0xtuba@protonmail.com>
2023-02-19 14:28:08 +0800

    wip for views

--- aggregators/views.py
+++ aggregators/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import ArcadeLoan, BenddaoBorrow, BenddaoLiquidate, BenddaoRedeem, NftfiLoan, NftfiLiquidated, NftfiRepaid, NftfiRenegotiated, X2Y2Loan, X2Y2Liquidation, X2Y2NonceCancelled, X2Y2Repaid
+
+
+def loans(request):
+    x2 = list(X2Y2Loan.objects.values().order_by('-loan_id'))
+    res = {
+        'data': x2,
+    }
+    return JsonResponse(res)
